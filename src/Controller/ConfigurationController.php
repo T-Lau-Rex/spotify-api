fb1d70314294fb47c5ea54179f392f1b9cd94523
cortesSecondary <jaumej07@gmail.com>
2023-12-15 16:29:35 +0100

    PodcastController and groups configuration

--- src/Controller/ConfigurationController.php
+++ src/Controller/ConfigurationController.php
@@ -3,9 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\BrowserKit\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class ConfigurationController extends AbstractController
 {
-    
+    public function configuraciones(Request $request, SerializerInterface $serializer)
+    {
+        
+    }
 }
