85de41b6364a416bd807aa3b37931f6b78302377
laura <la.calle.de.la.piruleta@hotmail.com>
2024-01-18 20:20:25 +0100

    2 primeras rutas de CancionController

--- src/Controller/CancionController.php
+++ src/Controller/CancionController.php
@@ -10,7 +10,7 @@ use Symfony\Component\Serializer\SerializerInterface;
 
 class CancionController extends AbstractController
 {
-    public function canciones(Request $request, SerializerInterface $serializer)
+    public function canciones(SerializerInterface $serializer)
     {
         // path: /canciones
         $canciones = $this->getDoctrine()
@@ -30,6 +30,16 @@ class CancionController extends AbstractController
         // path: /cancion/{id}
         $id = $request->get("id");
 
+        $cancion = $this->getDoctrine()
+            ->getRepository(Cancion::class)
+            ->findOneBy(['id' => $id]);
+
+        $cancion = $serializer->serialize(
+            $cancion,
+            'json',
+            ['groups' => ['cancion']]
+        );
+        return new Response($cancion);
     }
     public function canciones_playlist(Request $request, SerializerInterface $serializer)
     {
